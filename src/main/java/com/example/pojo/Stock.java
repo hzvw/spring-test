f7511b7a92a5494b78d09c7b2d324102d4a28ad7
zhanghz1120 <zhanghz1120@foxmail.com>
2025-02-15 00:49:52 +0800

    20250215于广州

---
+++ src/main/java/com/example/pojo/Stock.java
@@ -0,0 +1,19 @@
+package com.example.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("db_stock")
+public class Stock {
+
+    @TableId
+    private Long id;
+
+    private String productCode;
+
+    private String stockCode;
+
+    private Integer count;
+}
